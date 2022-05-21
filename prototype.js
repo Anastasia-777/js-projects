16ca649eef936c484f54e0e849aee8952d5382fd
Anastasia <nastyadorohina2799@mail.ru>
2022-05-22 01:09:54 +0300

    new task

--- prototype.js
+++ prototype.js
@@ -17,3 +17,19 @@ let foo = function () {
 }
 
 foo.defer(5000);
+
+/* Задача 2. Создать класс объекта Car с тремя свойствами : name, model, color.
+ Создать экземпляр класса с конкретными значениями свойств. Затем через созданный экземпляр добавить к классу свойство owner
+ с конкретным значением по умолчанию (Ivanov). Вывести все значения свойств созданного экземпляра */
+
+class Car {
+    constructor(name, model, color) {
+        this.name = name;
+        this.model = model;
+        this.color = color;
+    }
+}
+const myCar = new Car('Audi', 'A6', 'black');
+myCar.owner = 'Ivanov';
+
+console.log(`${myCar.name}, ${myCar.model}, ${myCar.color}, ${myCar.owner}`);
